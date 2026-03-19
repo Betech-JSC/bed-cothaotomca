d1dc9ea696307642d68fbff1d86e2a3b01e0eac1
TrungThanh-Cao <dev.thanhcao@gmail.com>
2026-03-20 00:35:13 +0700

    [Fix - FE] Bug policy

--- src/app/[locale]/policy/page.tsx
+++ src/app/[locale]/policy/page.tsx
@@ -2,7 +2,10 @@ import { redirect } from "@/i18n/routing";
 
 export default function PolicyIndexPage() {
   redirect({
-    pathname: '/policy/[slug]',
-    params: { slug: 'chinh-sach-giao-hang' }
+    href: {
+      pathname: '/policy/[slug]',
+      params: { slug: 'chinh-sach-giao-hang' }
+    },
+    locale: 'vi'
   });
 }
