37bad93dbf33083617a9810a744b2b57f6f72ba7
TrungThanh-Cao <dev.thanhcao@gmail.com>
2026-03-25 15:41:22 +0700

    Push code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config = {
     screens: {
       md: "768px",
       lg: "1024px",
-      xl: "1440px",
+      xl: "1280px",
       '2xl': "1920px",
       "max-2xl": { max: "1919px" },
-      "max-xl": { max: "1439px" },
+      "max-xl": { max: "1279px" },
       "max-lg": { max: "1023px" },
       "max-md": { max: "767px" },
     },
